b70fd6870252e6958ab3a45eb3f6c5aea97eafaf
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-05-26 14:05:00 +0530

    upd: bg + hero

--- src/lib/components/landing/HeroSection.svelte
+++ src/lib/components/landing/HeroSection.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	import cube_gif from '$lib/imgs/gif/cube5.gif';
+	import cube_gif from '$lib/imgs/gif/black_cube.gif';
 </script>
 
 <section id="hero" class="relative z-30 mx-auto mt-14 md:mt-24 lg:mt-32 max-w-7xl px-6 text-center md:px-8">
 	<div class="hero-container relative flex flex-col-reverse lg:flex-row xl:-translate-x-16">
 		<div class="left-container relative lg:w-[70%]">
 			<h1
-				class="-translate-y-4 animate-fade-in text-balance font-poppins lg:text-right bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text pt-6 text-5xl font-semibold leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] dark:from-white dark:to-white/40 sm:text-6xl md:text-7xl lg:text-8xl"
+				class="-translate-y-4 animate-fade-in text-balance font-poppins lg:text-right bg-gradient-to-br from-[var(--black-0)] from-30% to-[var(--white-0)]/40 bg-clip-text pt-6 text-5xl font-semibold leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] dark:from-[var(--black-0)] dark:to-[var(--black-0)]/40 sm:text-6xl md:text-7xl lg:text-8xl"
 			>
 				QuantumNLP
 			</h1>
 			<h2
-				class="-translate-y-4 animate-fade-in text-balance font-poppins lg:text-right bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text pb-6 pt-2 text-2xl font-medium leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] dark:from-white dark:to-white/40 sm:text-3xl md:text-4xl lg:text-5xl"
+				class="-translate-y-4 animate-fade-in text-balance font-poppins lg:text-right bg-gradient-to-br from-[var(--white-0)] from-30% to-[var(--white-0)]/40 bg-clip-text pb-6 pt-2 text-2xl font-medium leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] dark:from-[var(--black-0)] dark:to-[var(--black-0)]/5 sm:text-3xl md:text-4xl lg:text-5xl"
 			>
 				Integrating Quantum Computing with Natural Language Processing
 			</h2>
